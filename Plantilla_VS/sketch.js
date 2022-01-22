cd5db269366cec658dddc41bb9cbf6ba2fd1ea0d
Mcampa <92349250+mcampa21@users.noreply.github.com>
2022-01-22 13:17:53 -0700

    Update sketch.js

--- Plantilla_VS/sketch.js
+++ Plantilla_VS/sketch.js
@@ -5,8 +5,7 @@ function setup() {
   //Diseño de area de juego
   createCanvas(400,400);
   //Diseñoi de objeto 
- pelota=createSprite(200,200,50,50);
-
+  pelota=createSprite(200,200,50,50);
 }
 
 //Funcion de dibujo
@@ -14,23 +13,28 @@ function draw()
 {
   //Establecer color de fondo
   background(51);
-if (keyDown("d")) {
-  pelota.position.x=pelota.position.x+5;
-  background("red");
-}
-//Codigo para proyectar objetos
-if (keyDown("a")){
-  pelota.position.x=pelota.position.x-5;
-  background("blue");
-}
-if (keyDown("w")){
-  pelota.position.y=pelota.position.y-5;
-  background("yellow");
-}
-if (keyDown("s")){
-  pelota.position.y=pelota.position.y+5;
-  background("green");
-}
-drawSprites();
+  
+  if (keyDown("d")) {
+    pelota.position.x=pelota.position.x+5;
+    background("red");
+  }
+  
+  //Codigo para proyectar objetos
+  if (keyDown("a")) {
+    pelota.position.x=pelota.position.x-5;
+    background("blue");
+  }
+  
+  if (keyDown("w")) {
+    pelota.position.y=pelota.position.y-5;
+    background("yellow");
+  }
+  
+  if (keyDown("s")) {
+    pelota.position.y=pelota.position.y+5;
+    background("green");
+  }
+  
+  drawSprites();
 }
 
